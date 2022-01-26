1eb39413a844678ff8018415d7f8532f1ebd1783
sabbytech <80594124+sabbytech@users.noreply.github.com>
2022-01-26 10:10:21 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-//Next class
+function Check(){
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/omAF1sqCU/model.json', Model);
+}
+
+
+function Model(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error,results) {
+    console.log(results);
+}
